b2031c4087a4a2e19ec1ed97c18cff84ac4a88f3
gamepass2344-lgtm <gamepass2344@gmail.com>
2026-02-05 12:43:48 -0500

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,57 +2,64 @@ import React from "react";
 import './Home.css';
 import BannerImage from '../../assets/banner/banner-img.png';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChartLine,faShieldHalved,faChartArea} from "@fortawesome/free-solid-svg-icons";
-
+import { faChartLine, faShieldHalved, faChartArea } from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
 
-        
-          //Mobile Icon
-          const faChartIcon = <FontAwesomeIcon icon={faChartLine} />;
-          const faShieldIcon = <FontAwesomeIcon icon={faShieldHalved} />;
-          const faChartAreaIcon = <FontAwesomeIcon icon={faChartArea} />;
-
+  // Keep mobile icons
+  const faChartIcon = <FontAwesomeIcon icon={faChartLine} />;
+  const faShieldIcon = <FontAwesomeIcon icon={faShieldHalved} />;
+  const faChartAreaIcon = <FontAwesomeIcon icon={faChartArea} />;
 
   return (
     <>
       <section id="home">
         <div className="banner_image"></div>
+
         <div className="container">
           <div className="banner_outer">
+
             <div className="col">
               <h3 className="title">
-                WE PROMOTE YOUR <span>BUSINESS</span>
+                The <span>Family Plumbers</span>
               </h3>
-              <p>
-                We help businesses unlock their true potential with strategic insights and innovative marketing solutions. Let’s elevate your brand and connect with the right audience—your success starts here!
-              </p>
-              <div className="btn_wrapper">
-                <a className="btn" href="/">
-                  Get Started
-                </a>
-              </div>
-            </div>
-            <div className="col">
-                <div className="sub_banner_image">
-                        <img src={BannerImage} alt="Banner_image"/>
-                </div>
-                <div className="banner_style_1">
-                        {faChartIcon}
-                        <h4>Business Analysis</h4>
-                </div>
-                <div className="banner_style_1 banner_style_2 ">
-                        {faShieldIcon}
-                        <h4>99.9% Success</h4>
-                </div>
-                <div className="banner_style_1 banner_style_3">
-                        {faChartAreaIcon}
-                        <h4>Strategy</h4>
-                </div>
 
+              <p>
+                In business for over 25+ years — perfectionists that get the job done right.
+              </p>
+            </div>
+
+            <div className="col">
+              <div className="sub_banner_image">
+                <img src={BannerImage} alt="Banner_image"/>
+              </div>
+
+              {/* Keeping mobile style floating boxes */}
+              <div className="banner_style_1">
+                {faChartIcon}
+                <h4>Reliable Service</h4>
+              </div>
+
+              <div className="banner_style_1 banner_style_2">
+                {faShieldIcon}
+                <h4>Licensed & Insured</h4>
+              </div>
+
+              <div className="banner_style_1 banner_style_3">
+                {faChartAreaIcon}
+                <h4>25+ Years Experience</h4>
+              </div>
 
             </div>
           </div>
+
+          {/* Button moved to bottom */}
+          <div className="bottom_cta">
+            <a className="btn" href="#contact">
+              Get Started
+            </a>
+          </div>
+
         </div>
       </section>
     </>
